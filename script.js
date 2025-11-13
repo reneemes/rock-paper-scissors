1a91fd47c42c0367f8e211a1faf9104fb6614fb7
Renee Messersmith <reneemessersmith@Administrators-MacBook-Pro.local>
2025-11-13 16:05:13 -0500

    Feat: added scoring section, updated inner text, added scoring logic, helper methods for dry code

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 const gameForm = document.getElementById('game-form');
 const userInput = document.getElementById('user-input');
 const resultText = document.querySelector('.result-sec__result');
+const computerResult = document.querySelector('.result-sec__com-choice');
+const userResult = document.querySelector('.result-sec__user-choice');
+
+const userScoreBox = document.getElementById('user-score')
+const computerScoreBox = document.getElementById('computer-score')
+
+let userScore = 0;
+let computerScore = 0;
+
+userScoreBox.textContent = userScore;
+computerScoreBox.textContent = computerScore;
 
 const computerOptions = ['rock', 'paper', 'scissors'];
 
@@ -14,28 +25,52 @@ gameForm.addEventListener('submit', (e) => {
     return;
   }
 
-  checkWinner(userInput.value)
+  playGame(userInput.value);
   userInput.value = '';
 });
 
-function checkWinner(userTurn) {
+function playGame(userTurn) {
   let compT = computerTurn();
   let userT = userTurn.toLowerCase();
 
   if (userT === compT) {
-    console.log("It's a tie!")
+    tie(compT);
+    gameChoices(compT, userT);
   } else if (
     (userT === 'rock' && compT === 'scissors') ||
     (userT === 'paper' && compT === 'rock') ||
     (userT === 'scissors' && compT === 'paper')
   ) {
-    console.log('You win!')
+    winner()
+    gameChoices(compT, userT);
+    userScore += 1;
+    userScoreBox.textContent = userScore;
   } else {
-    console.log('Computer wins!')
+    loser();
+    gameChoices(compT, userT)
+    computerScore += 1;
+    computerScoreBox.textContent = computerScore;
   }
 };
 
 function computerTurn() {
   let randomValue = computerOptions[Math.floor(computerOptions.length * Math.random())];
   return randomValue
+};
+
+function tie() {
+  resultText.textContent = "It's a tie!";
+};
+
+function winner() {
+  resultText.textContent = 'You win!';
+};
+
+function loser() {
+  resultText.textContent = 'Computer wins!';
+}
+
+function gameChoices(comp, user) {
+  computerResult.textContent = `Computer's choice: ${comp}.`;
+  userResult.textContent = `Your choice: ${user}.`
 };
