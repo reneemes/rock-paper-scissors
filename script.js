b0e84f022068b8e471a3f4df9782231aa464d4bc
Renee Messersmith <reneemessersmith@AdminisatorsMBP.lan>
2025-11-14 17:20:40 -0500

    Feat: flipped and rotated icons for user and computer, removed result text, used inner HTML for changing icons

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
+// Form
 const gameForm = document.getElementById('game-form');
 const userInput = document.getElementById('user-input');
+// Text Outputs
 const resultText = document.querySelector('.result-sec__result');
-const computerResult = document.querySelector('.result-sec__com-choice');
-const userResult = document.querySelector('.result-sec__user-choice');
-
-const userScoreBox = document.getElementById('user-score')
-const computerScoreBox = document.getElementById('computer-score')
+// const computerResult = document.querySelector('.result-sec__com-choice');
+// const userResult = document.querySelector('.result-sec__user-choice');
+// Hand Icons
+// const userHand = document.getElementById('user-hand');
+// const computerHand = document.getElementById('computer-hand');
+const userHandSec = document.querySelector('.user-hand-sec');
+const computerHandSec = document.querySelector('.computer-hand-sec');
+// Score Board
+const userScoreBox = document.getElementById('user-score');
+const computerScoreBox = document.getElementById('computer-score');
 
 let userScore = 0;
 let computerScore = 0;
@@ -17,6 +24,8 @@ const computerOptions = ['rock', 'paper', 'scissors'];
 
 gameForm.addEventListener('submit', (e) => {
   e.preventDefault();
+  userHandSec.innerHTML = '';
+  computerHandSec.innerHTML = '';
 
   if (!computerOptions.includes(userInput.value.trim().toLowerCase())) {
     console.error('Unexpected value', userInput);
@@ -35,7 +44,7 @@ function playGame(userTurn) {
 
   if (userT === compT) {
     setResult("It's a tie!");
-    gameChoices(compT, userT);
+    setIcons(userT, compT);
   } else if (
     (userT === 'rock' && compT === 'scissors') ||
     (userT === 'paper' && compT === 'rock') ||
@@ -43,15 +52,13 @@ function playGame(userTurn) {
   ) {
     setResult('You win!')
     updateScore('user');
-    // userScore++;
-    // userScoreBox.textContent = userScore;
+    setIcons(userT, compT);
   } else {
     setResult('Computer wins!');
     updateScore('computer');
-    // computerScore++;
-    // computerScoreBox.textContent = computerScore;
+    setIcons(userT, compT);
   }
-  gameChoices(compT, userT);
+  // gameChoices(compT, userT);
 };
 
 function computerTurn() {
@@ -60,6 +67,32 @@ function computerTurn() {
 
 function setResult(message) {
   resultText.textContent = message;
+};
+
+const userRock = '<i class="fa-solid fa-hand-back-fist fa-rotate-90 fa-5x"></i>';
+const userPaper = '<i class="fa-solid fa-hand fa-rotate-90 fa-5x"></i>';
+const userScissors = '<i class="fa-solid fa-hand-scissors fa-flip-horizontal fa-5x"></i>';
+
+const computerRock = '<div class="flip"><i class="fa-solid fa-hand-back-fist fa-flip-horizontal fa-5x"></i></div>';
+const computerPaper = '<div class="flip"><i class="fa-solid fa-hand fa-flip-horizontal fa-5x"></i></div>';
+const computerScissors = '<i class="fa-solid fa-hand-scissors fa-5x"></i>';
+
+function setIcons(userT, compT) {
+  if (userT === 'rock') {
+    userHandSec.insertAdjacentHTML('beforeend', userRock);
+  } else if (userT === 'paper') {
+    userHandSec.insertAdjacentHTML('beforeend', userPaper);
+  } else {
+    userHandSec.insertAdjacentHTML('beforeend', userScissors);
+  }
+
+  if (compT === 'rock') {
+    computerHandSec.insertAdjacentHTML('beforeend', computerRock);
+  } else if (compT === 'paper') {
+    computerHandSec.insertAdjacentHTML('beforeend', computerPaper);
+  } else {
+    computerHandSec.insertAdjacentHTML('beforeend', computerScissors);
+  }
 };
 
 function updateScore(player) {
